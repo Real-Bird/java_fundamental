0a4c2f37f1ddd10ff7fe5e2000a20806306d1c82
RealBird <marbil815@gmail.com>
2021-05-30 19:24:22 +0900

    21-05-28-h2

--- src/java_20210528/EmpDao.java
+++ src/java_20210528/EmpDao.java
@@ -20,170 +20,175 @@ public class EmpDao {
 		}
 		return single;
 	}
+
 	public boolean insert(EmpDto dto) {
 		boolean success = false;
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		StringBuilder sql = new StringBuilder();
-		
+
 		try {
-			con = DriverManager.getConnection("jdbc:mariadb://localhost/kpc","kpc12","kpc1212");
+			con = DriverManager.getConnection("jdbc:mariadb://localhost/kpc", "kpc12", "kpc1212");
 			sql.append("insert into emp(empno, ename, job, mgr, hiredate, sal, comm, deptno) ");
-			sql.append("values (?, ?, ?, ?, ?, ?, ?, ?) ");
-			
+			sql.append("values (?, ?, ?, ?, curdate(), ?, ?, ?) ");
+
 			pstmt = con.prepareStatement(sql.toString());
-			
+
 			int index = 1;
 			pstmt.setInt(index++, dto.getNo());
 			pstmt.setString(index++, dto.getName());
 			pstmt.setString(index++, dto.getJob());
 			pstmt.setInt(index++, dto.getMgr());
-			pstmt.setString(index++, dto.getHiredate());
 			pstmt.setDouble(index++, dto.getSal());
 			pstmt.setDouble(index++, dto.getComm());
 			pstmt.setInt(index++, dto.getDeptno());
-			
+
 			pstmt.executeUpdate();
 			success = true;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
-				try {
-					if(con != null)
+			try {
+				if (con != null)
 					con.close();
 				if (pstmt != null)
 					pstmt.close();
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-		
-		
+
 		return success;
 	}
+
 	public boolean update(EmpDto dto) {
 		boolean success = false;
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		StringBuilder sql = new StringBuilder();
-		
+
 		try {
-			con = DriverManager.getConnection("jdbc:mariadb://localhost/kpc","kpc12","kpc1212");
+			con = DriverManager.getConnection("jdbc:mariadb://localhost/kpc", "kpc12", "kpc1212");
 			sql.append("update emp ");
 			sql.append("set ename = ?, job = ? ");
 			sql.append("where empno = ? ");
-			
+
 			pstmt = con.prepareStatement(sql.toString());
-			
+
 			int index = 1;
 			pstmt.setString(index++, dto.getName());
 			pstmt.setString(index++, dto.getJob());
 			pstmt.setInt(index++, dto.getNo());
-			
+
 			pstmt.executeUpdate();
 			success = true;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
-				try {
-					if(con != null)
+			try {
+				if (con != null)
 					con.close();
 				if (pstmt != null)
 					pstmt.close();
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-		
-		
+
 		return success;
 	}
+
 	public boolean delete(int no) {
 		boolean success = false;
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		StringBuilder sql = new StringBuilder();
-		
+
 		try {
-			con = DriverManager.getConnection("jdbc:mariadb://localhost/kpc","kpc12","kpc1212");
+			con = DriverManager.getConnection("jdbc:mariadb://localhost/kpc", "kpc12", "kpc1212");
 			sql.append("delete from emp ");
 			sql.append("where empno = ? ");
-			
+
 			pstmt = con.prepareStatement(sql.toString());
-			
+
 			int index = 1;
 			pstmt.setInt(index++, no);
-			
+
 			pstmt.executeUpdate();
 			success = true;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
-				try {
-					if(con != null)
+			try {
+				if (con != null)
 					con.close();
 				if (pstmt != null)
 					pstmt.close();
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		return success;
 	}
+
 	public ArrayList<EmpDto> select(int start, int len) {
 		ArrayList<EmpDto> list = new ArrayList<EmpDto>();
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		StringBuilder sql = new StringBuilder();
 		ResultSet rs = null;
-		
+
 		try {
-			con = DriverManager.getConnection("jdbc:mariadb://localhost/kpc","kpc12","kpc1212");
-			sql.append("select ename, job, hiredate, sal, deptno ");
+			con = DriverManager.getConnection("jdbc:mariadb://localhost/kpc", "kpc12", "kpc1212");
+			sql.append("select empno, ename, job, mgr, date_format(hiredate, '%Y/%m/%d'), sal, nvl(comm, 0), deptno ");
 			sql.append("from emp ");
-			sql.append("where ename like '%I%' ");
-			sql.append("order by sal DESC ");
+			sql.append("order by hiredate DESC ");
 			sql.append("limit ?, ? ");
-			
+
 			pstmt = con.prepareStatement(sql.toString());
-			
+
 			int index = 1;
 			pstmt.setInt(index++, start);
 			pstmt.setInt(index++, len);
-			
+
 			rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				index = 1;
+				int no = rs.getInt(index++);
 				String name = rs.getString(index++);
 				String job = rs.getString(index++);
+				int mgr = rs.getInt(index++);
 				String hiredate = rs.getString(index++);
 				double sal = rs.getDouble(index++);
+				double comm = rs.getDouble(index++);
 				int deptno = rs.getInt(index++);
-				list.add(new EmpDto(name, job, hiredate, sal, deptno));
+				list.add(new EmpDto(no, name, job, mgr, hiredate, sal, comm, deptno));
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
-				try {
-					if(con != null)
+			try {
+				if (con != null)
 					con.close();
 				if (pstmt != null)
 					pstmt.close();
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+				if (rs != null)
+					rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-		
+
 		return list;
 	}
-	
+
 }
--- src/java_20210528/EmpDto.java
+++ src/java_20210528/EmpDto.java
@@ -5,7 +5,7 @@ public class EmpDto {
 	private String name;
 	private String job;
 	private int mgr;
-	private String hiredate;
+	private String hiredate; // insert '2021-05-28' select date_format()
 	private double sal;
 	private double comm;
 	private int deptno;
