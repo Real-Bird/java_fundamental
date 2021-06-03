6d2a178e06b12834049ec3e9026d2c2bae9d1ca0
Real-Bird <marbil815@gmail.com>
2021-06-03 12:49:26 +0900

    21-06-03-v1

--- src/java_20210602/InterCalcDemo.java
+++ src/java_20210602/InterCalcDemo.java
@@ -1,12 +1,19 @@
 package java_20210602;
 
+import java_20210603.DelayInterCalc;
+
 public class InterCalcDemo {
 	public static void main(String[] args) {
 		DayCalc dc = new DayCalc();
-
+		DelayInterCalc dic = new DelayInterCalc();
+		
 		int dayDiff = dc.dayDiff("2020-05-17", "2021-01-27");
 		int dayInter = dc.dayInter(5000000, 0.18);
-
-		System.out.printf("투자 기간은 총 %d일이며, 이자는 총 %,d원입니다.", dayDiff, dayInter * dayDiff);
+		int delayDiff = dic.dayDiff("21-01-28",	"21-06-03");
+		int delayInter = dic.dayInter(5000000, 0.2);
+		
+		System.out.printf("투자 기간은 총 %d일이며, 이자는 총 %,d원입니다.%n", dayDiff, dayInter * dayDiff);
+		System.out.printf("연체 기간은 총 %d일이며, 연체이자는 총 %,d원입니다.%n", delayDiff, delayInter * delayDiff);
+		System.out.printf("정상 기간과 연체 기간의 총 투자일은 %d일이며,%n총 합산 이자는 %,d원입니다.%n", dayDiff+delayDiff, dayInter*dayDiff + delayInter*delayDiff);
 	}
 }
---
+++ src/java_20210603/DelayInterCalc.java
@@ -0,0 +1,43 @@
+package java_20210603;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DelayInterCalc {
+	Calendar sDate;
+	Calendar eDate;
+	int principal;
+	double interRate;
+	static double tax = 0.154;
+
+	public DelayInterCalc() {
+		sDate = Calendar.getInstance();
+		eDate = Calendar.getInstance();
+	}
+
+	public int dayDiff(String startDate, String endDate) {
+		try {
+			Date s_date = new SimpleDateFormat("yyyy-MM-dd").parse(startDate);
+			sDate.setTime(s_date);
+			Date e_date = new SimpleDateFormat("yyyy-MM-dd").parse(endDate);
+			eDate.setTime(e_date);
+
+			int diffDays = (int) (((eDate.getTimeInMillis() - sDate.getTimeInMillis())) / 1000 / (24 * 60 * 60));
+			return diffDays;
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return 0;
+		}
+	}
+
+	public int dayInter(int principal, double interRate) {
+		double beforeTax = principal * interRate / 365;
+		double fxxkingTax = beforeTax * tax;
+		int dailyInter = (int) (beforeTax - fxxkingTax);
+		return dailyInter;
+	}
+
+}
